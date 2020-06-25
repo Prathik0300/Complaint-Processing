7b50f7b2895f6458804029148b22fec07c1e3ed2
Prathik Pugazhenthi <prathik0300@gmail.com>
2020-06-26 02:22:16 +0530

    signUp confirm boxes Completed

--- script.js
+++ script.js
@@ -1,27 +1,74 @@
-
 function confirmation(){
-    window.location = "index.html";
-}
+    
+var emailId = document.getElementById("setUsername").value;
+var pass = document.getElementById("setPassword").value;
+var retypePass = document.getElementById("retypePassword").value;
+var cpf = document.getElementById("cpf").value;
+var quarterNo = document.getElementById("quarterNo").value;
+var name = document.getElementById("Name").value;
+var errors = [];
+    
+    if ((/^[a-zA-Z]+\s?[a-zA-Z]*$/.test(name))==false){
+        var nameErr = "Name Invalid";
+        errors.push(nameErr);
+    }
+    
+    if((/^[1-9]{1}\d+$/.test(cpf))==false){
+        var cpfErr = "CPF value invalid!";
+        errors.push(cpfErr);
+    }
+    
+   if((/^\w+([-\.]\w)*[@]{1}[a-zA-Z]+[\.]{1}[A-Za-z]{2,3}$/).test(emailId)==false){
+        var emailErr = "Email entered Invalid";
+        errors.push(emailErr);
+    }
+    
+    if(pass !== retypePass){
+        var passErr = "Passwords are not same!";
+        errors.push(passErr);
+    }
+    
+    if((/^[1-9]{1}\d+$/.test(quarterNo))==false){
+        var quarterErr = "Quarter No is invalid!";
+        errors.push(quarterErr);
+    }
+    
+    
+    
+    if (errors.length==0){
 
-var myConfirm = new function(){
+    var msg = "Are you sure to continue with the Email-ID : " + emailId.toLowerCase();    
+    var res = confirm(msg);
     
-    this.show = function(msg,callback){
-        document.getElementById("alert-message").style.top="-30rem";
-        document.getElementById("alert-message").style.display="block";
-        document.getElementById("freezelayer").style.zIndex="2";
-        document.getElementById("signIn-row").style.zIndex="1";
-        this.callback = callback;
-    };
+    if (res){
+     window.open("index.html","_self");
+    }
     
-    this.okay = function(){
-        this.callback();
-        this.close();
-    };
+   else{
+       window.close(res);
+   }
     
-    this.close =function(){
-        document.getElementById("alert-message").style.top="-32rem";
-        document.getElementById("alert-message").style.display="none";
-        document.getElementById("freezelayer").style.zIndex="1";
-        document.getElementById("signIn-row").style.zIndex="2";
+}
+    
+    else{
+        var l = errors.length;
+        var error = "";
+        for (var i=0;i<l;i++){
+            error = error + errors[i] + " ";
+        }
+        
+        var errMsg = "There are " + error + " errors. Rectify it and try again";
+        
+        var message = confirm(errMsg);
+    
+        if (message){
+            window.close(message);
+        }
+    
+        else{
+            window.open("index.html","_self");
+            
+        }
+        
     }
 }
